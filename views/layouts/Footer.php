760bb3e65c182bb8359a556bab9559310985f48c
FathimaJasama <jasamafathima01@gmail.com>
2024-03-11 12:17:50 +0530

    login page is created

---
+++ views/layouts/Footer.php
@@ -0,0 +1,35 @@
+
+
+		<!-- *************
+			************ Required JavaScript Files *************
+		************* -->
+		<!-- Required jQuery first, then Bootstrap Bundle JS -->
+		<script src="<?=asset("assets/js/jquery.min.js")?>"></script>
+		<script src="<?=asset("assets/js/bootstrap.bundle.min.js")?>"></script>
+		<script src="<?=asset("assets/js/modernizr.js")?>"></script>
+		<script src="<?=asset("assets/js/moment.js")?>"></script>
+
+		<!-- *************
+			************ Vendor Js Files *************
+		************* -->
+
+		<!-- Overlay Scroll JS -->
+		<script src="<?=asset("assets/vendor/overlay-scroll/jquery.overlayScrollbars.min.js")?>"></script>
+		<script src="<?=asset("assets/vendor/overlay-scroll/custom-scrollbar.js")?>"></script>
+
+		<!-- News ticker -->
+		<script src="<?=asset("assets/vendor/newsticker/newsTicker.min.js")?>"></script>
+		<script src="<?=asset("assets/vendor/newsticker/custom-newsTicker.js")?>"></script>
+
+		<!-- Date Range JS -->
+		<script src="<?=asset("assets/vendor/daterange/daterange.js")?>"></script>
+		<script src="<?=asset("assets/vendor/daterange/custom-daterange.js")?>"></script>
+
+		<!-- Dropzone JS -->
+		<script src="<?=asset("assets/vendor/dropzone/dropzone.min.js")?>"></script>
+
+		<!-- Main Js Required -->
+		<script src="<?=asset("assets/js/main.js")?>"></script>
+	</body>
+
+</html>
